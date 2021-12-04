a0ecafc72d02674af5353ebbb917ecb66fc6983c
OMG <stansky666@gmail.com>
2021-12-04 14:17:18 +0100

    Starting Projet

---
+++ lib/constants/constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xffF9F9F9);
+const kWhiteColor = Color(0xffffffff);
+const kOrangeColor = Color(0xffEF716B);
+const kBlueColor = Color(0xff4B7FFB);
+const kYellowColor = Color(0xffFFB167);
+const kTitleTextColor = Color(0xff1E1C61);
+const kSousTitleTextColor = Color(0xFF2E2C70);
+const kSearchBackgroundColor = Color(0xffF2F2F2);
+const kSearchTextColor = Color(0xffC0C0C0);
+const kCategoryTextColor = Color(0xff292685);

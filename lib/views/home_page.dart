88a5b24f000c37b6f3ee33004b35b907bd33125b
OMG <stansky666@gmail.com>
2021-12-06 10:44:26 +0100

    Ajoute des quick actions

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_omg_note_sqflite/main.dart';
+import 'package:quick_actions/quick_actions.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +10,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final quickActions = const QuickActions();
+
+  @override
+  void initState() {
+    super.initState();
+
+    quickActions.setShortcutItems([
+      const ShortcutItem(
+        type: "write",
+        localizedTitle: "Nouvelle note",
+      ),
+      const ShortcutItem(
+        type: "scanne",
+        localizedTitle: "Scanner un document",
+      ),
+      const ShortcutItem(
+        type: "search",
+        localizedTitle: "Rechercher",
+      ),
+    ]);
+
+    quickActions.initialize((type) {
+      if (type == "write") {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const MyApp(),
+          ),
+        );
+      }
+
+      if (type == "scanne") {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const MyApp(),
+          ),
+        );
+      }
+
+      if (type == "search") {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const MyApp(),
+          ),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

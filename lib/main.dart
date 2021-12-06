42720e254f6ee4fe9ecc13d73ae3e2e9b22bab4b
OMG <stansky666@gmail.com>
2021-12-06 10:57:19 +0100

    Ajout du plashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_omg_note_sqflite/views/home_page.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Widget splash = SplashScreenView(
+      navigateRoute: const HomePage(),
+      duration: 3000,
+      imageSize: 200,
+      imageSrc: 'assets/logo.png',
+      text: "Omg Note",
+      textType: TextType.ColorizeAnimationText,
+      textStyle: const TextStyle(
+        fontSize: 40.0,
+        fontWeight: FontWeight.bold,
+      ),
+      colors: const [
+        Colors.purple,
+        Colors.blue,
+        Colors.yellow,
+        Colors.red,
+      ],
+      backgroundColor: Colors.white,
+    );
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'OmgNote',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: splash,
     );
   }
 }
